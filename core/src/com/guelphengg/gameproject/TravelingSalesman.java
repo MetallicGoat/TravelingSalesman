5d2bcaf88d8ec4c1a3808a9f9a3e7eea4a8d817a
Mark de Lima <mark.de.lima@outlook.com>
2024-02-19 17:23:26 -0500

    Trying to integrate movements with a new playing field... But mostly trying to make a home page.

--- core/src/com/guelphengg/gameproject/TravelingSalesman.java
+++ core/src/com/guelphengg/gameproject/TravelingSalesman.java
@@ -40,33 +40,7 @@ public class TravelingSalesman extends ApplicationAdapter {
 		font = new BitmapFont();
 	}
 
-	public class Rectangle extends Actor {
-
-		private Texture texture;
-
-		public Rectangle(float x, float y, float width, float height, Color color) {
-			createTexture((int)width, (int)height, color);
-			setX(x);
-			setY(y);
-			setWidth(width);
-			setHeight(height);
-		}
-
-		private void createTexture(int width, int height, Color color) {
-			Pixmap pixmap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
-			pixmap.setColor(color);
-			pixmap.fillRectangle(0, 0, width, height);
-			texture = new Texture(pixmap);
-			pixmap.dispose();
-		}
-
-		@Override
-		public void draw(Batch batch, float parentAlpha) {
-			Color color = getColor();
-			batch.setColor(color.r, color.g, color.b, color.a * parentAlpha);
-			batch.draw(texture, getX(), getY(), getWidth(), getHeight());
-		}
-	}
+	
 	@Override
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
