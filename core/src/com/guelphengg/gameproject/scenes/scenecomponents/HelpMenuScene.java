ce4988dad59b2f0a8bd3f58c7bef3f38b36ad31a
sethd <sethdevries040@gmail.com>
2024-03-15 12:11:41 -0400

    first couple changes to the help menu

---
+++ core/src/com/guelphengg/gameproject/scenes/scenecomponents/HelpMenuScene.java
@@ -0,0 +1,44 @@
+package com.guelphengg.gameproject.scenes.scenecomponents;
+
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.guelphengg.gameproject.GameState;
+import com.guelphengg.gameproject.scenes.Scene;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.guelphengg.gameproject.Accessor;
+import com.guelphengg.gameproject.GameManager;
+import com.guelphengg.gameproject.SceneManager;
+//this scene is for a help menu with game instructions
+public class HelpMenuScene extends Scene {
+
+    public HelpMenuScene(){super(GameState.HELP_MENU);}
+
+    @Override
+    public void render() {
+        renderBackground(0.5f);
+
+        SpriteBatch batch = new SpriteBatch();
+        BitmapFont font = new BitmapFont();
+
+        batch.begin();
+
+        drawCenteredText(batch, 100, 4,"Test");
+
+        batch.end();
+    }
+
+    private void drawCenteredText(SpriteBatch batch, int yOffset, float scale, String text) {
+        final BitmapFont font = SceneManager.getFont();
+        final GlyphLayout glyphLayout = new GlyphLayout();
+
+        font.getData().setScale(scale);
+        glyphLayout.setText(font, text);
+
+        float w = glyphLayout.width;
+        float h = glyphLayout.height;
+
+        // draw magic (some mathies to find the center)
+        font.draw(batch, glyphLayout, (SceneManager.getViewWidth() - w)/2, (SceneManager.getViewHeight() + h) / 2 + yOffset);
+    }
+}
