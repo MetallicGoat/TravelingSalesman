a2cd6682662c79f92d2c54d6dd69c7e62d54121a
MetallicGoat <the.great.metallic.goat@gmail.com>
2024-03-16 16:05:45 -0400

    Simplify Generation

--- core/src/com/guelphengg/gameproject/Generation.java
+++ core/src/com/guelphengg/gameproject/Generation.java
@@ -1,165 +1,79 @@
 package com.guelphengg.gameproject;
 
 import com.guelphengg.gameproject.griditems.GridObject;
-import com.guelphengg.gameproject.GameManager;
 import java.util.Random;
 
 public class Generation {
 
     public static void generateLandmarks(){
-        Random randNum = new Random();
-        GridObject[][] gridObjects = Accessor.getGameManager().getGridObjectArray();
+        final Random randNum = new Random();
+        final GridObject[][] gridObjects = Accessor.getGameManager().getGridObjectArray();
 
-        //generates a random position for the castle in the middle 4 squares of the grid
-        int castleRow = randNum.nextInt(4,6);
-        int castleCol = randNum.nextInt(4,6);
-        gridObjects[castleRow][castleCol] = GridObject.CASTLE;
+        // generates a random position for the castle in the middle 4 squares of the grid
+        final int castleRow = randNum.nextInt(4,6);
+        final int castleCol = randNum.nextInt(4,6);
 
-        //generates random ints between 0-9
-        int randRow;
-        int randCol;
+        gridObjects[castleRow][castleCol] = GridObject.CASTLE; // Only one castle
 
-        //array of all structures in the game
-        GridObject[] gridObjList = {GridObject.TREASURE_HOUSE, GridObject.TRAPPED_HOUSE, GridObject.LOST_ITEM_HOUSE, GridObject.MARKET};
+        // Generate in quadrant 1
+        for (int i = 1; i <=4; i++) {
 
+            // 2 treasure houses per quadrant (8 total)
+            generateObject(i, GridObject.TREASURE_HOUSE);
+            generateObject(i, GridObject.TREASURE_HOUSE);
 
-        //logic for generating treasure houses
-        int numTreasureHouses = 2;
-        //Generate in quadrant 1
-        for(int i = 0; i < numTreasureHouses; i++) {
-            randRow = randNum.nextInt(0,5);
-            randCol = randNum.nextInt(0,5);
-            if (gridObjects[randRow][randCol] == null) {
-                gridObjects[randRow][randCol] = gridObjList[0];
-            }
-        }
-        //Generate in Quad 2
-        for(int i = 0; i < numTreasureHouses; i++) {
-            randRow = randNum.nextInt(5,10);
-            randCol = randNum.nextInt(0,5);
-            if (gridObjects[randRow][randCol] == null) {
-                gridObjects[randRow][randCol] = gridObjList[0];
-            }
-        }
-        //Generate in Quad 3
-        for(int i = 0; i < numTreasureHouses; i++) {
-            randRow = randNum.nextInt(0,5);
-            randCol = randNum.nextInt(5,10);
-            if (gridObjects[randRow][randCol] == null) {
-                gridObjects[randRow][randCol] = gridObjList[0];
-            }
-        }
-        //Generate in Quad 4
-        for(int i = 0; i < numTreasureHouses; i++) {
-            randRow = randNum.nextInt(5,10);
-            randCol = randNum.nextInt(5,10);
-            if (gridObjects[randRow][randCol] == null) {
-                gridObjects[randRow][randCol] = gridObjList[0];
-            }
-        }
+            // 3 lost item houses per quadrant (12 total)
+            generateObject(i, GridObject.LOST_ITEM_HOUSE);
+            generateObject(i, GridObject.LOST_ITEM_HOUSE);
+            generateObject(i, GridObject.LOST_ITEM_HOUSE);
 
-        //logic for generating trapped houses
-        int numTrappedHouses = 1;
-        //Generate in quadrant 1
-        for(int i = 0; i < numTrappedHouses; i++) {
-            randRow = randNum.nextInt(0,5);
-            randCol = randNum.nextInt(0,5);
-            if (gridObjects[randRow][randCol] == null) {
-                gridObjects[randRow][randCol] = gridObjList[1];
-            }
-        }
-        //Generate in Quad 2
-        for(int i = 0; i < numTreasureHouses; i++) {
-            randRow = randNum.nextInt(5,10);
-            randCol = randNum.nextInt(0,5);
-            if (gridObjects[randRow][randCol] == null) {
-                gridObjects[randRow][randCol] = gridObjList[1];
-            }
-        }
-        //Generate in Quad 3
-        for(int i = 0; i < numTreasureHouses; i++) {
-            randRow = randNum.nextInt(0,5);
-            randCol = randNum.nextInt(5,10);
-            if (gridObjects[randRow][randCol] == null) {
-                gridObjects[randRow][randCol] = gridObjList[1];
-            }
-        }
-        //Generate in Quad 4
-        for(int i = 0; i < numTreasureHouses; i++) {
-            randRow = randNum.nextInt(5,10);
-            randCol = randNum.nextInt(5,10);
-            if (gridObjects[randRow][randCol] == null) {
-                gridObjects[randRow][randCol] = gridObjList[1];
-            }
-        }
+            // 1 trapped house per quadrant (4 total)
+            generateObject(i, GridObject.TRAPPED_HOUSE);
 
-        //logic for generating lost items
-        int numLostItems = 4;
-        //Generate in quadrant 1
-        for(int i = 0; i < numLostItems; i++) {
-            randRow = randNum.nextInt(0,5);
-            randCol = randNum.nextInt(0,5);
-            if (gridObjects[randRow][randCol] == null) {
-                gridObjects[randRow][randCol] = gridObjList[2];
-            }
-        }
-        //Generate in Quad 2
-        for(int i = 0; i < numLostItems; i++) {
-            randRow = randNum.nextInt(5,10);
-            randCol = randNum.nextInt(0,5);
-            if (gridObjects[randRow][randCol] == null) {
-                gridObjects[randRow][randCol] = gridObjList[2];
-            }
-        }
-        //Generate in Quad 3
-        for(int i = 0; i < numLostItems; i++) {
-            randRow = randNum.nextInt(0,5);
-            randCol = randNum.nextInt(5,10);
-            if (gridObjects[randRow][randCol] == null) {
-                gridObjects[randRow][randCol] = gridObjList[2];
-            }
-        }
-        //Generate in Quad 4
-        for(int i = 0; i < numLostItems; i++) {
-            randRow = randNum.nextInt(5,10);
-            randCol = randNum.nextInt(5,10);
-            if (gridObjects[randRow][randCol] == null) {
-                gridObjects[randRow][randCol] = gridObjList[2];
-            }
-        }
+            // 1 market per quadrant (4 total)
+            generateObject(i, GridObject.MARKET);
 
-        //logic for generating markets
-        int numMarkets = 1;
-        //Generate in quadrant 1
-        for(int i = 0; i < numMarkets; i++) {
-            randRow = randNum.nextInt(0,5);
-            randCol = randNum.nextInt(0,5);
-            if (gridObjects[randRow][randCol] == null) {
-                gridObjects[randRow][randCol] = gridObjList[3];
-            }
         }
-        //Generate in Quad 2
-        for(int i = 0; i < numMarkets; i++) {
-            randRow = randNum.nextInt(5,10);
-            randCol = randNum.nextInt(0,5);
-            if (gridObjects[randRow][randCol] == null) {
-                gridObjects[randRow][randCol] = gridObjList[3];
+    }
+
+    // Warning this will cause and infinite loop if the quadrant is completely full
+    private static void generateObject(int quadrant, GridObject object) {
+        final GridObject[][] gridObjects = Accessor.getGameManager().getGridObjectArray();
+        final Random randNum = new Random();
+
+        boolean success = false;
+
+        // Keep generating until we find a spot that is empty
+        while (!success) {
+            final int randRow;
+            final int randCol;
+
+            switch (quadrant) {
+                case 1:
+                    randRow = randNum.nextInt(0, 5);
+                    randCol = randNum.nextInt(0, 5);
+                    break;
+                case 2:
+                    randRow = randNum.nextInt(5, 10);
+                    randCol = randNum.nextInt(0, 5);
+                    break;
+                case 3:
+                    randRow = randNum.nextInt(0, 5);
+                    randCol = randNum.nextInt(5, 10);
+                    break;
+                case 4:
+                    randRow = randNum.nextInt(5, 10);
+                    randCol = randNum.nextInt(5, 10);
+                    break;
+                default:
+                    randRow = 0;
+                    randCol = 0;
+                    break;
             }
-        }
-        //Generate in Quad 3
-        for(int i = 0; i < numMarkets; i++) {
-            randRow = randNum.nextInt(0,5);
-            randCol = randNum.nextInt(5,10);
+
             if (gridObjects[randRow][randCol] == null) {
-                gridObjects[randRow][randCol] = gridObjList[3];
-            }
-        }
-        //Generate in Quad 4
-        for(int i = 0; i < numMarkets; i++) {
-            randRow = randNum.nextInt(5,10);
-            randCol = randNum.nextInt(5,10);
-            if (gridObjects[randRow][randCol] == null) {
-                gridObjects[randRow][randCol] = gridObjList[3];
+                gridObjects[randRow][randCol] = object;
+                success = true;
             }
         }
     }
