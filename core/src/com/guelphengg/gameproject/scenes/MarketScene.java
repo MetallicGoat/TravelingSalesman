e4a46f8d8f6ab8e70e428ea1620bb4a97128a2b2
Mark de Lima <mark.de.lima@outlook.com>
2024-03-26 11:47:19 -0400

    Added MarketScene.java.

---
+++ core/src/com/guelphengg/gameproject/scenes/MarketScene.java
@@ -0,0 +1,12 @@
+package com.guelphengg.gameproject.scenes;
+
+import com.guelphengg.gameproject.griditems.LootItems;
+
+public class MarketScene {
+    LootItems item_1;
+    LootItems item_2;
+    LootItems item_3;
+    LootItems item_4;
+
+    // Are we just doing weapons for this?
+}
